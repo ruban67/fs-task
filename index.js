f3dd9de45d21882ad4ec04caafe072e3a1e4a307
Ruban_p <rubansmash17@gmail.com>
2023-02-12 16:52:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const app = express()
+
+let date_time = new Date();
+
+
+let date = ("0" + date_time.getDate()).slice(-2);
+
+// get current month
+let month = ("0" + (date_time.getMonth() + 1)).slice(-2);
+
+// get current year
+let year = date_time.getFullYear();
+
+// get current hours
+let hours = date_time.getHours();
+
+// get current minutes
+let minutes = date_time.getMinutes();
+
+// get current seconds
+let seconds = date_time.getSeconds();
+
+// prints date in YYYY-MM-DD format
+console.log(year + "-" + month + "-" + date);
+
+// prints date & time in YYYY-MM-DD HH:MM:SS format
+console.log(year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds);
